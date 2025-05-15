7b94eeffab2fdba9ad28920e6fdc2a9ba992944a
Iamshchikov.Vladimir <yamshikovvl@gmail.com>
2025-05-16 02:02:25 +0400

    fix

--- src/shared/api/axiosInstance.js
+++ src/shared/api/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-const {VITE_API_URL} = import.meta.env;
 
 const axiosInstance = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: 'https://pet-shop-backend-e104.onrender.com',
 });
 
 export default axiosInstance;
